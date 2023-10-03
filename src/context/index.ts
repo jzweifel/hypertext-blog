9c98682d0d7877e243731d81ceb7b1c8a0c23a8e
Jacob Zweifel <jacob@jacobzweifel.com>
2023-10-03 15:28:14 -0500

    add more logging

--- src/context/index.ts
+++ src/context/index.ts
@@ -1,5 +1,19 @@
+import { logger } from "@bogeychan/elysia-logger";
 import { html } from "@elysiajs/html";
+import { HoltLogger } from "@tlscipher/holt";
 import { Elysia } from "elysia";
+import pretty from "pino-pretty";
+import { auth } from "../auth";
+import { config } from "../config";
+
+const stream = pretty({
+  colorize: true,
+});
+
+const loggerConfig =
+  config.env.NODE_ENV === "development"
+    ? { level: config.env.LOG_LEVEL, stream }
+    : { level: config.env.LOG_LEVEL };
 
 export const ctx = new Elysia({
   name: "@app/ctx",
@@ -13,4 +27,11 @@ export const ctx = new Elysia({
     console.log("auth time", performance.now() - now, "ms");
 
     return { session };
-  });
+  })
+  .use(logger(loggerConfig))
+  .use(
+    // @ts-expect-error
+    config.env.NODE_ENV === "development"
+      ? new HoltLogger().getLogger()
+      : (a) => a,
+  );
