30be5a2bbcee94ee9e4caf31784946eb1e7c576e
Jacob Zweifel <jacob@jacobzweifel.com>
2023-10-08 14:35:02 -0500

    remove sentry replay

--- src/components/base.tsx
+++ src/components/base.tsx
@@ -16,7 +16,6 @@ export const BaseHtml = ({ children }: Html.PropsWithChildren) => (
             dsn: "https://a0654b7e9a0d42e0dd8f25332ae9ca6b@o4506004695482368.ingest.sentry.io/4506004856373248",
             replaysSessionSampleRate: 1.0,
             replaysOnErrorSampleRate: 1.0,
-            integrations: [new Sentry.Replay()],
           });
       `}
       </script>
